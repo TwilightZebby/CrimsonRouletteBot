b8a286c9560cc47fd041078926ffe72d8e71316c
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2020-02-25 10:07:34 +0000

    Quick edits to remove copies of my TwilightBot Code

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,6 @@ module.exports = {
     description: 'Pong, divide by 2?',
     usage: ' ',
     commandType: 'general',
-    guildAccess: 'all',
     execute(message) {
       //>>>>>>>>>> Return Heartbeat Ping <<<<<<<<<<
       return message.reply(`Pong! \n Your ping is ${message.client.ws.ping.toFixed(2)}ms`);
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const fs = require('fs'); // Node's native file system
 const Discord = require("discord.js"); // Bringing in Discord.js
 const { client } = require('./bot_modules/constants.js'); // Brings in the Discord Bot's Client and Sequelize Database
-const { PRIVATE, TRUSTED } = require('./bot_modules/guilds.js'); // Used for checking Command Access Perms per Guild
 const { PREFIX, TOKEN } = require('./config.js'); // Slapping the PREFIX and token into their own vars
 client.commands = new Discord.Collection(); // Extends JS's native map class
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); // Picks up all the .js files in the commands folder
@@ -41,155 +40,6 @@ client.on("ready", () => {
 
 
 
-/***********************************************/
-// Birthday Role stuff
-// Just for my private server with my IRL friends
-// Yes, we have a Birthday Role lol
-let userStore = new Map();
-
-client.on('guildMemberUpdate', (oldMember, newMember) => {
-
-  // First, ensure this is the correct Server
-  if ( oldMember.guild.id !== '156482432902758400' || newMember.guild.id !== '156482432902758400' ) {
-    return;
-  }
-  // Next, ignore any Bot Users
-  if ( oldMember.user.bot || newMember.user.bot ) {
-    return;
-  }
-
-  // Set User into Map for timing reasons
-  let bUser;
-  bUser = userStore.get(newMember.user.id);
-  if ( !bUser ) {
-    let bUserConstruct = {
-      name: newMember.displayName,
-      id: newMember.user.id,
-      timeout: null
-    };
-    userStore.set(newMember.user.id, bUserConstruct);
-  }
-
-
-  // Now, check for the highest Role and see if that is the Birthday Role!
-  const birthdayRole = newMember.guild.roles.get('286566932629422084'); // Grab the Role Object
-  const announceChannel = newMember.guild.channels.get('156482432902758400'); // Grab the Channel to announce in!
-
-  if ( ( newMember.roles.highest === birthdayRole ) && ( !oldMember.roles.has(birthdayRole) ) ) {
-    
-    const roleEmbed = new Discord.MessageEmbed().setColor(birthdayRole.hexColor).setFooter(`IT'S ${newMember.displayName}'S BIRTHDAY YO`);
-
-    roleEmbed.addField(`It's a Birthday! 🎉`, `Hey, it's ${newMember}'s Birthday today!\n\n${newMember} - you can use the Role Command to change the name and colour of ${birthdayRole}!\n\nUse \`${PREFIX}help role\` to see how`);
-    announceChannel.send(roleEmbed);
-
-
-    // Start 24hour timer
-    // This is to auto-remove Role from User after 24 hours
-    birthdayUser = userStore.get(newMember.user.id);
-    birthdayUser.timeout = client.setTimeout(function(){
-
-      let bMember = announceChannel.members.get(birthdayUser.id);
-      bMember.roles.remove(birthdayRole);
-
-      // END of Timeout for birthdayUser
-    }, 8.64e+7);
-
-  }
-
-
-});
-
-
-
-
-
-/***********************************************/
-/*Voice Channel Stuff*/
-/*Runs whenever Bot detects someone joining/leaving a VC*/
-client.on('voiceStateUpdate', (oldVoiceState, newVoiceState) => {
-
-  // Overwrites/Ignores
-  //     Used for making the Bot ignore myself (TwilightZebby) and Bot Users
-  if ( ( oldVoiceState.member.id === '156482326887530498' || newVoiceState.member.id === '156482326887530498' ) || ( oldVoiceState.member.user.bot === true || newVoiceState.member.user.bot === true ) || ( oldVoiceState.member.id === oldVoiceState.guild.ownerID || newVoiceState.member.id === newVoiceState.guild.ownerID ) ) {
-    return;
-  }
-
-
-  // FUNCTIONS
-  //    Function for revoking a Member "Read Messages" Permission for a Text Channel
-  function RevokePermission(memberObject, textChannelID, guildObject) {
-
-    const channel = guildObject.channels.get(textChannelID);
-    channel.createOverwrite(memberObject, {
-      VIEW_CHANNEL: false,
-    }).catch(error => console.error(error));
-
-  }
-
-  //    Function for granting a Member "Read Messages" Permission for a Text Channel
-  function GrantPermission(memberObject, textChannelID, guildObject) {
-
-    const channel = guildObject.channels.get(textChannelID);
-    channel.createOverwrite(memberObject, {
-      VIEW_CHANNEL: true,
-    }).catch(error => console.error(error));
-
-  }
-  
-
-
-  // WHEN USER LEAVES A VOICE CHANNEL
-  if ( (oldVoiceState.channelID !== undefined || oldVoiceState.channelID !== null) && (newVoiceState.channelID === undefined || newVoiceState.channelID === null) ) {
-
-
-    const member = newVoiceState.member;
-
-    // FOR MY PRIVATE SERVER
-    if (member.guild.id === '264091258035634176') {
-      RevokePermission(member, '664911349243183104', member.guild);
-    }
-
-    // FOR MY CLOSE FRIENDS SERVER
-    if (member.guild.id === '156482432902758400') {
-      RevokePermission(member, '618520093558571018', member.guild);
-    }
-
-
-
-  } 
-  
-  // WHEN USER JOINS A VOICE CHANNEL
-  if ( (oldVoiceState.channelID === undefined || oldVoiceState.channelID === null) && (newVoiceState.channelID !== undefined || newVoiceState.channelID !== null) ) {
-
-
-    const member = oldVoiceState.member;
-
-    // FOR MY PRIVATE SERVER
-    if (member.guild.id === '264091258035634176') {
-      GrantPermission(member, '664911349243183104', member.guild);
-    }
-
-    // FOR MY CLOSE FRIENDS SERVER
-    if (member.guild.id === '156482432902758400') {
-      GrantPermission(member, '618520093558571018', member.guild);
-    }
-
-
-  }
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
 
 /***********************************************/
 /*THE COMMANDS*/
@@ -209,6 +59,9 @@ client.on("message", async (message) => {
   // If there is NOT a command with the given name or aliases, exit early
   const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
   if (!command) return;
+
+
+
 
   // COOLDOWNS
   // If a command has 'cooldown: x,' it will enable cooldown IN SECONDS
@@ -231,6 +84,11 @@ client.on("message", async (message) => {
      timestamps.set(message.author.id, now);
      setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
    }
+
+
+
+
+
 
   // A check for if the user ran a command inside DMs
   // if a cmd has 'guildOnly: true,', it won't work in DMs
@@ -255,18 +113,11 @@ client.on("message", async (message) => {
       return message.channel.send(reply);
   }
 
-  // Check Command's Guild Access
-  if (command.guildAccess === 'private' && message.guild.id != PRIVATE) {
 
-    console.log('Private Command Attempted in invalid Guild');
-    return message.reply(`Sorry, but that command cannot be used in this Server!\nIt is limited to <@156482326887530498>\'s private server.\n*No, you are not getting access to it*`);
-  
-  } else if (command.guildAccess === 'trusted' && !TRUSTED.includes(message.guild.id)) {
 
-    console.log('Trusted Command Attempted in invalid Guild');
-    return message.reply(`Sorry, but that command cannot be used in this Server!\nIt is limited to Servers <@156482326887530498> has trusted.`);
+
+
   
-  }
 
   // If there is, grab and run that command's execute() function
   try {

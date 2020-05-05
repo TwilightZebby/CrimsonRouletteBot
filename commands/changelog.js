8b6a6f615ac889a207b58db76b77f64a68d4d725
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2020-05-05 09:17:46 +0100

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -19,35 +19,36 @@ module.exports = {
       const updateEmbed = new Discord.MessageEmbed().setColor('#07f51b').setFooter('v1.2.1 Changelog');
 
       // Date calculation
-      let updateDate = Date.parse("11:30 April 27, 2020");
+      let updateDate = Date.parse("8:17 May 5, 2020");
       updateEmbed.setTimestamp(updateDate);
 
       
 
       // Changelog itself
-      updateEmbed.setTitle(`v1.2.1 - WIP`);
+      updateEmbed.setTitle(`v1.2.1 - 5th May 2020`);
 
 
       // Variables so that this is easier for me to read in this code form ;P
-      let additionArray = [
-        `+ `
-      ];
+      //let additionArray = [
+      //  `+ `
+      //];
       
       let changeArray = [
         `• Fixed incorrect Level Up/Down Messages when caused by a Roulette Command`,
         `• All Command's Cooldowns have had backend tweaks to them. Shouldn't affect you as the User much`,
-        `• Lowered the chances of getting negative results from either Roulette Commands by a tiny bit. You shouldn't notice much change`
+        `• Lowered the chances of getting negative results from either Roulette Commands by a tiny bit. You shouldn't notice much change`,
+        `• Updated \`info\` command to use new \`discord.com\` domain instead of \`discordapp.com\` for Bot's Invite Link`
       ];
       
-      let removeArray = [
-        `- `
-      ];
+      //let removeArray = [
+      //  `- `
+      //];
 
 
       updateEmbed.addFields(
-        { name: `Additions`, value: additionArray.join(`\n`) },
-        { name: `Changes`, value: changeArray.join(`\n`) },
-        { name: `Removals`, value: removeArray.join(`\n`) }
+        //{ name: `Additions`, value: additionArray.join(`\n`) },
+        { name: `Changes`, value: changeArray.join(`\n`) }
+        //{ name: `Removals`, value: removeArray.join(`\n`) }
       )
 
       return message.channel.send(updateEmbed);

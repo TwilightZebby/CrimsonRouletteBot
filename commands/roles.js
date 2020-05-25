8a6734093d7e49f8bc97c97cd5b986c193fe1e7a
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2020-05-25 11:04:47 +0100

    Added catch for Guild Owners
    (Yes I forgot this bit!)

--- commands/roles.js
+++ commands/roles.js
@@ -17,6 +17,13 @@ module.exports = {
     const configEmbed = new Discord.MessageEmbed().setColor('#07f51b').setFooter('Levelling Role Config Module');
 
 
+    // Only the Guild Owner can use this command (and myself)
+    if (message.author.id !== message.guild.ownerID || message.author.id !== "156482326887530498") {
+      configEmbed.setTitle(`Something went wrong....`);
+      configEmbed.setDescription(`Sorry, but only the Guild/Server Owner can use this command!`);
+      return message.channel.send(configEmbed);
+    }
+
 
     if (!args.length) {
 

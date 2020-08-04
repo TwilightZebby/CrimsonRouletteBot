038997ec002f39efc682f44afd202e4c03c3e7d6
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2020-08-04 08:21:18 +0100

    v1.4.3 Version bump

--- commands/changelog.js
+++ commands/changelog.js
@@ -16,29 +16,29 @@ module.exports = {
       PREFIX = await functFile.LoadPrefix(message.guild.id, ConfigData);
 
 
-      const updateEmbed = new Discord.MessageEmbed().setColor('#07f51b').setFooter('v1.4.1 Changelog');
+      const updateEmbed = new Discord.MessageEmbed().setColor('#07f51b').setFooter('v1.4.3 Changelog');
 
       // Date calculation
       // YYYY-MM-DD T HH:MM:SS
       // End with "Z" for UTC time
       // For a specific timezone, replace "Z" with "+HH:MM" or "-HH:MM"
-      let updateDate = Date.parse("2020-08-03T10:40:00+01:00");
+      let updateDate = Date.parse("2020-08-04T08:20:00+01:00");
       updateEmbed.setTimestamp(updateDate);
 
       
 
       // Changelog itself
-      updateEmbed.setTitle(`v1.4.2 - 3rd August 2020`);
+      updateEmbed.setTitle(`v1.4.3 - 4th August 2020`);
 
 
       // Variables so that this is easier for me to read in this code form ;P
-      let additionArray = [
+      /*let additionArray = [
         `+ Added a new \`allowMentions\` option in the \`c!prefs\` command - this controls if the Bot should \`@mention\` you in Level Up/Down messages or not.`
-      ];
-      
-      /*let changeArray = [
-        `• `
       ];*/
+      
+      let changeArray = [
+        `• Fixed a bug which caused the "You need to be the Server Owner" Error to appear for the \`config\`, \`roles\`, and \`reset\` commands even if you own that Server`
+      ];
       
       /*let removeArray = [
         `- `
@@ -46,8 +46,8 @@ module.exports = {
 
 
       updateEmbed.addFields(
-        { name: `Additions`, value: additionArray.join(`\n`) },
-        //{ name: `Changes`, value: changeArray.join(`\n`) },
+        //{ name: `Additions`, value: additionArray.join(`\n`) },
+        { name: `Changes`, value: changeArray.join(`\n`) },
         //{ name: `Removals`, value: removeArray.join(`\n`) }
       )
 
